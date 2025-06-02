6aa1a511b7cefd1f651b7d783527c9be781a7399
Jeremy <jeremyjosecastellanos@gmail.com>
2025-06-01 20:12:18 -0600

    Creación de una pagina de Error 404, y Agregar DefaultLayout y manejar ruta 404
    - Crear componente DefaultLayout que envuelve TopNavbar, <Outlet/> y Footer
    - Mover Home, Blogs y About como rutas hijas dentro de DefaultLayout
    - Añadir <Route path="*" element={<Error404 />} /> para mostrar página de error 404 en rutas inválidas
    - Dejar /login como ruta independiente sin Layout

--- src/App.js
+++ src/App.js
@@ -1,36 +1,49 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import TopNavbar from './components/TopNavbar';
 import Login from './pages/Login';
 import Home from './pages/Home';
 import Blogs from './pages/masters/Create_Blogs/Blogs';
-import Footer from './components/Footer.js'
-import About from './pages/page_About/About.js';
+import Footer from './components/Footer';
+import About from './pages/page_About/About';
+import Error404 from './pages/Error';
+import { Outlet } from 'react-router-dom';
+
+// Este componente Layout solo coloca Navbar arriba, <Outlet/> en medio y Footer al final
+function DefaultLayout() {
+    return (
+        <div>
+            <TopNavbar />
+            <main style={{ padding: 0 }}>
+                {/* Aquí se renderizarán las rutas hijas */}
+                <Outlet />
+            </main>
+            <Footer />
+        </div>
+    );
+}
 
 function App() {
-    const [items, setItems] = useState([{
-        title: "Progra Web",
-        description: "Clase de los sabados"
-    }]);
     return (
         <Router>
             <Routes>
+                {/* Ruta independiente para /login */}
                 <Route path="/login" element={<Login />} />
-                <Route path="/*" element={
-                    <div>
-                        <TopNavbar />
-                            <div style={{
-                                padding: '0px'
-                            }}>
-                                <Routes>
-                                    <Route path="/*" element={<Home />} />
-                                    <Route path="/blogs" element={<Blogs />} />
-                                    <Route path="/about" element={<About />} />
-                                </Routes>
-                            </div>
-                        <Footer />
-                    </div>
-                } />
+
+                {/* Todas las demás rutas usan DefaultLayout */}
+                <Route element={<DefaultLayout />}>
+                    {/* Ruta de Home (/) */}
+                    <Route path="/" element={<Home />} />
+                    
+                    {/* Ruta de Blogs (/blogs) */}
+                    <Route path="/blogs" element={<Blogs />} />
+                    
+                    {/* Ruta de About (/about) */}
+                    <Route path="/about" element={<About />} />
+
+                    {/* Error 404: todo path que no haya matcheado arriba caerá aquí */}
+                    <Route path="*" element={<Error404 />} />
+                </Route>
             </Routes>
         </Router>
     );
