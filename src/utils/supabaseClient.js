b1ce825734db813707824749d4bc468a3f774e77
Jeremy <jeremyjosecastellanos@gmail.com>
2025-06-16 15:47:58 -0600

    Cree un archivo .env donde guarde variables de entorno (REACT_APP_SUPABASE_URL y REACT_APP_SUPABASE_ANON_KEY), y luego solo las llame a supabaseClient.js

--- src/utils/supabaseClient.js
+++ src/utils/supabaseClient.js
@@ -1,4 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-export const supabase = createClient("https://fxlzmkvkflbabmilexsi.supabase.co"
-    ,"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ4bHpta3ZrZmxiYWJtaWxleHNpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk1Nzg4OTksImV4cCI6MjA2NTE1NDg5OX0.DPdyGqh7M3I2s9Zs9Mz5traThBs5i38AHGCtWmr-8R0")
+export const supabase = createClient(
+    process.env.REACT_APP_SUPABASE_URL, 
+    process.env.REACT_APP_SUPABASE_ANON_KEY
+);
